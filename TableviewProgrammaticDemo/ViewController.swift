f39227d0f720206800d651d2955b6ca68d5c1630
Matthew Dolan <mdolancode@gmail.com>
2024-11-21 18:14:35 -0800

    Created a Tableview Programmatic Demo.

--- TableviewProgrammaticDemo/ViewController.swift
+++ TableviewProgrammaticDemo/ViewController.swift
@@ -7,13 +7,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+        // Add the tabeView to the view
+        view.addSubview(tableView)
+        
+        // Set constarints for the tableView
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        
+        // Set the dataSource and delegate
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        //  Register a default cell
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Row \(indexPath.row + 1)"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Selected row\(indexPath.row + 1)")
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
 
